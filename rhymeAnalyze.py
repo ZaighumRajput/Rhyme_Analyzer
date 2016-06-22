e7a0cfc53e38091efbffc0ace6da5a910e406a42
zaighumrajput <zaighum47@gmail.com>
2016-06-22 22:59:52 +0000

    response

--- rhymeAnalyze.py
+++ rhymeAnalyze.py
@@ -1,20 +1,32 @@
 import requests
-
+import pytest
 
 
 class datamuse_request_object():
+    response = []
+    apihook = "https://api.datamuse.com/"
+    status_code = "not connected"
 
     def __init__(self, query : str, search : str, code=None):
         self.apicall = "/words?{}={}".format(query, search)
 
-def datamuse_api(search : str):
-    Api_Url = "https://api.datamuse.com/"
-    r = requests.get(url)
-    return r.status_code
+    def connect(self):
+        r = requests.get(self.apihook + self.apicall)
+        self.status_code = r.status_code
 
-#def test_api_connection():
-    #assert datamuse_api(search="biggie") == 200
 
-def test_request_object_querystring():
-    first = datamuse_request_object("sl", "biggie")
-    first.apicall = "/words?sl=biggie"
+
+class Test_Datamust_request_object:
+
+    def test_apicall_string(cls):
+        first = datamuse_request_object("sl", "biggie")
+        assert first.apicall ==  "/words?sl=biggie"
+
+    def test_connection(cls):
+        first = datamuse_request_object("sl", "biggie")
+        first.connect()
+        assert first.status_code == 200
+
+    def test_response(cls):
+        first = datamuse_request_object("sl", "biggie")
+
